4cce8345dd4b98a72f1182b87bec1d6685b858d9
borishdez <borisnandez93@gmail.com>
2024-07-02 14:39:08 -0600

    pipes number

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
-
+  public totalSales:number = 2352536.5745;
+  public percent:number = 0.4563;
 }

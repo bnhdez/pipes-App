11b03eeeed518b2db48eda6ca104bd353a986f58
borishdez <borisnandez93@gmail.com>
2024-07-02 15:39:57 -0600

    pipe Async

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -39,4 +40,17 @@ export class UncommonPageComponent {
     age: 22,
     address: 'Santa Tecla, El Salvador'
   }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap', value) )
+  );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+      console.log('Tenemos data en la promesa.');
+      this.name = 'Otro nombre';
+    }, 3500);
+  })
 }

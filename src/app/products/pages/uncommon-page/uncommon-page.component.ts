2259dd9228cf86b1b3426409267d66e53188bfda
borishdez <borisnandez93@gmail.com>
2024-07-02 15:00:54 -0600

    pipe i18nSelect

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+  //i18nSelect
+  public name:string = 'Boris';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
 
+  changeClient():void{
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 }
